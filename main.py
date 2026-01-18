b17b3e723f6151eb76c6a794125a004e57bfbb64
trungn21 <trungn21@student.ubc.ca>
2026-01-17 20:09:34 -0800

    fix

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from config import *
 result = capture("test1.jpg", "./")
 print(result)
 PROMPT="Describe this image in less than 10 words."
-upload_image_to_gemini("test.jpg", PROMPT, GOOGLE_API_KEY)
+upload_image_to_gemini(result, PROMPT, GOOGLE_API_KEY)
